Spectral Analysis scaling issues fixed.

Code/bin/run_spectral_statistics.py
@@ -3,13 +3,20 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-import kickedrotor.perturbed_quantum_kicked_rotor as rotor
+import scipy.stats as stats
+import scipy.linalg as linalg
+import seaborn as sns
+# import kickedrotor.perturbed_quantum_kicked_rotor as rotor
+import kickedrotor.quasiperiodic_rotor_3d as rotor
 import kickedrotor.spectral_statistics as spectral
-
+import kickedrotor.params as params
 sns.set()
 
-F = rotor.denseFloquetOperator(deltak=0, deltatau=0)
-phases, num_discard = spectral.getPhaseSpectrum(F, tol=1e-9, discard=True)
+# F = rotor.denseFloquetOperator(deltak=0, deltatau=0)
+F = rotor.getFloquetOperator()
+# F = stats.unitary_group.rvs(1000)
+phases, eigvals, num_discard = spectral.getPhaseSpectrum(F, tol=0.1, discard=True)
+phases *= 1 / params.HBAR
 print(f"Number of Eigenphases in use = {phases.shape[0]}")
 print(f"Number of Discarded Phases = {num_discard}")
 spacings, ratios = spectral.getSpacingRatios(phases)
@@ -20,5 +27,5 @@ np.nan_to_num(ratios, copy=False, nan=-1, posinf=-1, neginf=-1)
 fig, (ax1, ax2) = plt.subplots(nrows=2, ncols=1, figsize=(8, 8))
 spectral.plotRatios(ratios, ax=ax1)
 spectral.plotSpacings(spacings, ax=ax2)
-plt.savefig("plots/kickedrotor_spectrum.png")
+plt.savefig(f"plots/quasiperiodic_kickedrotor_spectrum_N{params.N}_K{params.K}.png")
 plt.show()

Code/kickedrotor/bipartite_entanglement.py
@@ -10,36 +10,13 @@ Decisions: We use the momentum basis for all three dimensions. This is because
 """
 
 import numpy as np
-import scipy.fft as fft
 from scipy.special import xlogy, seterr
 from scipy.sparse import csr_matrix, csc_matrix
 from scipy.sparse import eye as sparse_eye
 from scipy.linalg import eigvals
 import matplotlib.pyplot as plt
-from cython.parallel import prange
-cimport cython
-
-from libc.math cimport sqrt, cos, sin, isnan
-from libc.stdio cimport printf
-
-cdef extern from "math.h":
-    double M_PI
-
-# Scientific Constants
-cdef double HBAR = 2.89
-cdef double K = 5
-cdef double ALPHA = 0.2
-cdef double OMEGA2 = 2 * M_PI * sqrt(5)
-cdef double OMEGA3 = 2 * M_PI * sqrt(13)
-cdef double complex I = 1j
-
-# Program Constants
-cdef int N = 10
-cdef int DIM = 2 * N + 1
-cdef double EPSILON = 1e-6
-cdef int TIMESTEPS = 20
-cdef FSAMPLES = 32
-DTYPE = np.complex128
+import kickedrotor.quasiperiodic_rotor_3d as floquet
+from kickedrotor.params import *
 
 def printMatrix(matrix, name):
     """
@@ -60,13 +37,6 @@ def printNaNInf(array, name):
     print(f"{name} has {nan_list[0].shape[0]} NaNs at {nan_list}")
     print(f"{name} has {inf_list[0].shape[0]} Infs at {inf_list}")
 
-def kickFunction(theta1, theta2, theta3):
-    """
-    Returns the kick part of floquet operator.
-    """
-    quasikick = (1 + ALPHA * np.cos(theta2) * np.cos(theta3))
-    return np.exp(-1j * K * np.cos(theta1) * quasikick / HBAR)
-
 def getInitialState():
     """
     Returns the initial state as tensor product of the 3 momentum spaces.
@@ -85,74 +55,13 @@ def getInitialDensity():
     state = getInitialState()
     return np.outer(state, state)
 
-def getKickFourierCoeffs(func):
-    """
-    Returns the 3d fourier coefficient matrix of the function kick(t1, t2, t3).
-    """
-    theta = np.arange(FSAMPLES) * 2 * np.pi / (FSAMPLES)
-    theta1, theta2, theta3 = np.meshgrid(theta, theta, theta)
-    x = func(theta1, theta2, theta3)
-    y = fft.fftshift(fft.fftn(x, norm="forward"))
-    return y.astype(DTYPE)
-
-cdef double complex complex_exp(double angle) nogil:
-    return <double> cos(angle) + I * <double> sin(angle)
-
-@cython.boundscheck(False)
-@cython.wraparound(False)
-def getDenseFloquetOperator(fourier_coeffs):
-    """
-    Returns the dense version of the floquet operator.
-    """
-
-    F = np.zeros((DIM**3, DIM**3), dtype=DTYPE)
-    cdef double complex[:, :] F_view = F
-    cdef double complex[:, :, :] fourier_view = fourier_coeffs
-
-    cdef int m1, m2, m3, n1, n2, n3
-    cdef int shift = FSAMPLES / 2
-    cdef int row, col
-    # m1 = m2 = m3 = n1 = n2 = n3 = row = col = 0
-    cdef double angle = 0.0
-    cdef double complex fourier = 0 + 0*I
-    cdef double complex phase
-
-    cdef int DIMSQ = DIM**2
-
-    for m1 in prange(-N, N+1, nogil=True):
-        for m2 in prange(-N, N+1):
-            for m3 in prange(-N, N+1):
-                for n1 in prange(-N, N+1):
-                    for n2 in prange(-N, N+1):
-                        for n3 in prange(-N, N+1):
-                            angle = (HBAR / 2) * n1**2 + n2 * OMEGA2 + n3 * OMEGA3
-                            phase = complex_exp(-angle)
-                            fourier = fourier_view[m3-n3+shift, m1-n1+shift, m2-n2+shift]
-                            row = (m1 + N) * DIMSQ + (m2 + N) * DIM + (m3 + N)
-                            col = (n1 + N) * DIMSQ + (n2 + N) * DIM + (n3 + N)
-                            F_view[row, col] = phase * fourier
-
-    return F
-
-
 def getFloquetOperator():
     """
     Returns the floquet operator for the 3d quasiperiodic kicked rotor.
     """
-    fourier_coeffs = getKickFourierCoeffs(kickFunction)
-    # fourier_coeffs[np.abs(fourier_coeffs) < EPSILON] = 0
-    # printNaNInf(fourier_coeffs, "fourier_coeffs")
-
-    F = getDenseFloquetOperator(fourier_coeffs)
-    # printNaNInf(F, "F")
+    F = floquet.getFloquetOperator()
     F.real[np.abs(F.real) < EPSILON] = 0
     F.imag[np.abs(F.imag) < EPSILON] = 0
-    # np.nan_to_num(F, copy=False, nan=0)
-    # printNaNInf(F, "F")
-    # printMatrix(F, "F")
-    # sign, slogdet = np.linalg.slogdet(F)
-    # print(f"det(F) = {sign} x exp({slogdet})")
-    # print(f"Eigenvalues of F are {np.sort(np.abs(eigvals(F)))}")
     Fh = np.conjugate(F.T)
     return csr_matrix(F), csr_matrix(Fh)
 

Code/kickedrotor/spectral_statistics.py
@@ -10,6 +10,9 @@ Purpose: Spectral Statistics
 import numpy as np
 import scipy.linalg as linalg
 import matplotlib.pyplot as plt
+import seaborn as sns
+
+CSE_SPACING_CONST = 2**18 / (3**6 * np.pi**3)
 
 def getPhaseSpectrum(F, tol=1e-9, discard=True):
     """
@@ -18,55 +21,131 @@ def getPhaseSpectrum(F, tol=1e-9, discard=True):
     eigs = linalg.eigvals(F)
     amplitudes = np.abs(eigs)
     phases = np.angle(eigs)
+    sort_key = np.argsort(phases)
+
+    eigs = eigs[sort_key]
+    amplitudes = amplitudes[sort_key]
+    phases = phases[sort_key]
+
     error = np.abs(amplitudes - 1)
     retainable = (error < tol)
     num_discard = eigs.shape[0] - np.count_nonzero(retainable)
     if discard:
-        return np.sort(phases[retainable]), num_discard
+        return phases[retainable], eigs[retainable], num_discard
     else:
-        return np.sort(phases)
-
+        return np.sort(phases), eigs
 
 def getSpacingRatios(eigenphases):
     """
     Returns the spacings and spacing ratios of the eigenphases
     of the given matrix.
     """
-    spacings = np.diff(eigenphases)
+    spacings = np.abs(np.diff(eigenphases))
+    spacings /= np.mean(spacings)
     ratios = spacings[1:] / spacings[:-1]
     return spacings, ratios
 
-def spacingSurmise(s):
+def spacingPoisson(s):
     """
-    Returns the value of P_W(s) the Wigner surmise for the GOE/COE
-    level spacings distribution.
+    Returns the expected Poisson distribution for level spacings
+    in integrable systems.
     """
-    return (np.pi / 2) * s * np.exp(-np.pi * s**2 / 4)
+    return np.exp(-s)
 
-def ratioSurmise(r):
+def spacingSurmiseCUE(s):
     """
-    Returns the value of P_W(r), the Wigner-like surmise for the
-    GOE/COE level spacing ratio distribution.
+    Returns the Wigner surmise for the spacings distribution
+    in CUE.
+    """
+    return (32/np.pi**2) * s**2 * np.exp(-s**2 * 4 / np.pi)
+
+def spacingSurmiseCOE(s):
+    """
+    Returns the Wigner surmise for the spacing distribution
+    in COE.
+    """
+    return (np.pi / 2) * s * np.exp(-s**2 * np.pi / 4)
+
+def spacingSurmiseCSE(s):
+    """
+    Returns the Wigner surmise for the spacing distribution
+    in CSE
+    """
+    return CSE_SPACING_CONST * s**4 * np.exp(-s**2 * 64 / (9 * np.pi))
+
+def ratioPoisson(r):
+    """
+    Returns the distribution of level spacings ratio for integrable
+    systems derived from the Poisson spacing distribution.
+    """
+    return 1 / (1 + r)**2
+
+def ratioSurmiseCUE(r):
+    """
+    Returns the Wigner-like surmise for the spacing ratio
+    distribution in CUE.
+    """
+    Z = 4 * np.pi / (81 * np.sqrt(3))
+    return (1/Z) * (r + r**2)**2 / (1 + r + r**2)**4
+
+def ratioSurmiseCOE(r):
+    """
+    Returns the Wigner-like surmise for the spacing ratio
+    distribution in COE.
     """
     Z = 8 / 27
     return (1/Z) * (r + r**2)**1 / (1 + r + r**2)**2.5
 
+def ratioSurmiseCSE(r):
+    """
+    Returns the Wigner-like surmise for the spacing ratio
+    distribution in CSE.
+    """
+    Z = 4 * np.pi / (729 * np.sqrt(3))
+    return (1/Z) * (r + r**2)**4 / (1 + r + r**2)**7
+
+
 def plotRatios(ratios, ax):
-    lims = (0, 0.5)
-    r = np.linspace(*lims, 100)
-    surmise = ratioSurmise(r)
-    ax.hist(ratios, density=True, bins=100, range=lims, histtype="step")
-    ax.plot(r, surmise)
+    # lims = (ratios.min(), ratios.max())
+    r = np.linspace(0, 5, 100)
+    poisson = ratioPoisson(r)
+    cue = ratioSurmiseCUE(r)
+    coe = ratioSurmiseCOE(r)
+    cse = ratioSurmiseCSE(r)
+    ax.hist(ratios, density=True, bins=100,
+        histtype="step", label="Calculated", range=(0, 5))
+    ax.plot(r, poisson, label="Poisson")
+    ax.plot(r, cue, label="CUE")
+    ax.plot(r, coe, label="COE")
+    ax.plot(r, cse, label="CSE")
     ax.set_xlabel(r"$r$")
     ax.set_ylabel(r"$P(r)$")
-    ax.set_yscale("log")
+    # ax.set_yscale("log")
+    ax.legend()
 
 def plotSpacings(spacings, ax):
-    lims = (spacings.min(), spacings.max())
-    s = np.linspace(*lims, 100)
-    surmise = spacingSurmise(s)
-    ax.hist(spacings, density=True, bins=100, histtype="step")
-    ax.plot(s, surmise)
+    # lims = (spacings.min(), spacings.max())
+    s = np.linspace(0, 5, 100)
+    poisson = spacingPoisson(s)
+    cue = spacingSurmiseCUE(s)
+    coe = spacingSurmiseCOE(s)
+    cse = spacingSurmiseCSE(s)
+
+    counts, bins = np.histogram(spacings)
+    total = sum(counts)
+    # print("counts:", counts)
+    # print("bins:", bins)
+    # bin_width = bins[1] - bins[0]
+    # counts = [count/total for count in counts]
+    # ax.bar(x=bins, height=counts, width=bin_width, label="Calculated", filled=False)
+
+    ax.hist(spacings, density=True, bins=100, range=(0,5),
+        histtype="step", label="Calculated")
+    ax.plot(s, poisson, label="Poisson")
+    ax.plot(s, cue, label="CUE")
+    ax.plot(s, coe, label="COE")
+    ax.plot(s, cse, label="CSE")
     ax.set_xlabel(r"$s$")
     ax.set_ylabel(r"$P(s)$")
-    ax.set_yscale("log")
+    # ax.set_yscale("log")
+    ax.legend()